b36783a63199cb2d91a6f770c0441c7e8dc24fb6
Akhil Krishna S <s.akhilkrishna@gmail.com>
2022-05-05 14:09:20 +0530

    Api

--- code/main.py
+++ code/main.py
@@ -1,14 +1,23 @@
 from typing import Optional
 import uvicorn
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Response, Request , Body
+from pydantic import BaseModel
+class data_req(BaseModel):
+    UID:str
+class data_res(BaseModel):
+    UID:str
+    message:str
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Alan is that you?"}
+    return {"Hello"}
 
+@app.post("/data")
+def return_UID(data:data_req): 
+    print (data)   
+    return{"UID":data.UID, "Message":"POST Success"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
